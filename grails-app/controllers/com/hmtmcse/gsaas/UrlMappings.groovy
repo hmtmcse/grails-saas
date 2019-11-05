2f71ddd879601bfb241388f90b918934f92a4704
Touhid Mia <hmtm.cse@gmail.com>
2019-11-05 06:38:23 +0600

    Documentation

--- grails-app/controllers/com/hmtmcse/gsaas/UrlMappings.groovy
+++ grails-app/controllers/com/hmtmcse/gsaas/UrlMappings.groovy
@@ -2,14 +2,5 @@ package com.hmtmcse.gsaas
 
 class UrlMappings {
 
-    static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
-
-        "500"(controller: 'exception', action: "error500")
-        "404"(view:'/notFound')
-    }
+    static mappings = {}
 }
